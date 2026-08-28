73a16a91c2b0bf5f314ee7218037a09c9b7f800c
amogu <d.krolikov@gmail.com>
2026-08-29 01:10:10 +0300

    Конвертация координат окна в координаты сетки

--- src/ColorPanel.java
+++ src/ColorPanel.java
@@ -4,6 +4,9 @@ import java.awt.image.BufferedImage;
 
 public class ColorPanel extends JPanel {
     private volatile BufferedImage imageToDraw;
+    private int scale = 1;
+    private int offsetX = 0;
+    private int offsetY = 0;
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -13,12 +16,12 @@ public class ColorPanel extends JPanel {
 
         double scaleX = (double) getWidth() / imageToDraw.getWidth();
         double scaleY = (double) getHeight() / imageToDraw.getHeight();
-        double scale = (int) Math.min(scaleX, scaleY); // один коэффициент чтобы пиксели сохранили квадратность
+        scale = (int) Math.min(scaleX, scaleY); // один коэффициент чтобы пиксели сохранили квадратность
 
         int drawWidth = (int) (imageToDraw.getWidth() * scale);
         int drawHeight = (int) (imageToDraw.getHeight() * scale);
-        int offsetX = (getWidth() - drawWidth) / 2;
-        int offsetY = (getHeight() - drawHeight) / 2; // считаем где рисовать изображение так как из-за одного коэффициента возникают пустые поля
+        offsetX = (getWidth() - drawWidth) / 2;
+        offsetY = (getHeight() - drawHeight) / 2; // считаем где рисовать изображение так как из-за одного коэффициента возникают пустые поля
 
         Graphics2D g2D = (Graphics2D) g;
         g2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR); // настраиваем растяжение без размытия
@@ -32,5 +35,15 @@ public class ColorPanel extends JPanel {
 
     public void setImageToDraw(BufferedImage imageToDraw) {
         this.imageToDraw = imageToDraw;
+    }
+
+    public Point panelCordsToGridCords(int x, int y) {
+        if (imageToDraw == null) return null;
+        if (!(x >= offsetX && y >= offsetY && x < imageToDraw.getWidth() * scale + offsetX && y < imageToDraw.getHeight() * scale + offsetY && scale != 0)) return null;
+
+        int gridX = (x - offsetX) / scale;
+        int gridY = (y - offsetY) / scale;
+
+        return new Point(gridX, gridY);
     }
 }
