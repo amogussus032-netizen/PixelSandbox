cbeeb62694cdfd10d0ce2c9528c3cbaf0ffe2eba
amogu <d.krolikov@gmail.com>
2026-08-30 00:34:09 +0300

    Добавлена простая симуляция песка к цвету 1

--- src/Simulation.java
+++ src/Simulation.java
@@ -3,7 +3,19 @@ public class Simulation {
 
     }
 
-    public static void step(double x) {
+    public static void step(double dt, PixelGrid simulatedGrid) {
+        int gridSizeX = simulatedGrid.getSizeX();
+        int gridSizeY = simulatedGrid.getSizeY();
 
+        for (int x = 0; x < gridSizeX; x++) {
+            for (int y = gridSizeY - 1; y >= 0; y--) {
+                if (simulatedGrid.getPixel(x, y) == (byte) 1) {
+                    if (simulatedGrid.getPixel(x, y + 1) == (byte) 0) {
+                        simulatedGrid.setPixel(x, y, (byte) 0);
+                        simulatedGrid.setPixel(x, y + 1, (byte) 1);
+                    }
+                }
+            }
+        }
     }
 }
